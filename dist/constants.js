6b8bdddca71ee26bf34998e4e5cbc32c3de991f7
Marc Espín Sanz <mespinsanz@gmail.com>
2020-09-01 00:06:37 +0200

    update

--- dist/constants.js
+++ dist/constants.js
@@ -1,4 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.valueOfPi = void 0;
 const valueOfPi = 3.14;
 exports.valueOfPi = valueOfPi;
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.twoPi = void 0;
 const constants_1 = require("./constants");
 exports.twoPi = constants_1.valueOfPi * 2;
